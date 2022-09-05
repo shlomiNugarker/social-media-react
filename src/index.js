6bdc9ec22c45dcbb47c58656eb21ebf168db3c2d
Shlomi Nugarker <shlomin1231@gmail.com>
2022-09-05 22:44:27 +0300

    added PWA

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { store } from './store'
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import {
@@ -92,6 +94,11 @@ root.render(
   </Provider>
   // </React.StrictMode>
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
