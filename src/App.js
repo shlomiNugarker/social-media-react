596b5d9d1f4b71e1efc0b53aa4b5d37299096a28
Shlomi Nugarker <shlomin1231@gmail.com>
2023-05-10 00:50:45 +0300

    mimor change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './assets/scss/global.scss'
-// import { Redirect } from 'react-router-dom'
+
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Main } from './pages/Main'
 import { About } from './pages/About'
 import { Signup } from './pages/Signup'
 import { getLoggedinUser } from '../src/store/actions/userActions'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 
 const App = () => {
