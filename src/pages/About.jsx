5d03720740aacd6f70e2092f9ff83cec8084c641
Shlomi Nugarker <shlomin1231@gmail.com>
2023-06-16 19:22:24 +0300

    iframe

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
 export function About() {
   return (
     <section className="about-page">
-      <h1>About!</h1>
+      <iframe
+        src="https://shlomi-nugarker-portfolio.vercel.app/projects"
+        height="90%"
+        width="90%"
+        title="hlomi-nugarker-portfolio.vercel.app/projects"
+      ></iframe>
     </section>
   )
 }
