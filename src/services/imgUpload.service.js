38fb584106e606fbb8707303bc6346e0fe062331
Shlomi Nugarker <shlomin1231@gmail.com>
2025-01-26 12:00:34 +0200

    use api to upload files

--- src/services/imgUpload.service.js
+++ src/services/imgUpload.service.js
@@ -1,41 +1,43 @@
-export const uploadImg = (ev) => {
-  //Defining our variables
-  const UPLOAD_PRESET = 'social_n_shlomi' //insert yours
-  const CLOUD_NAME = 'duajg3ah1' //insert yours
-  const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
-  const FORM_DATA = new FormData()
-  //Building the request body
-  FORM_DATA.append('file', ev.target.files[0]) // form data kind of key : file, add the file we get
-  FORM_DATA.append('upload_preset', UPLOAD_PRESET) // add the upload_preset
-  //Sending a post method request to Cloudinary`s API
-  return fetch(UPLOAD_URL, {
-    // add the img to Cloudinary
-    method: 'POST',
-    body: FORM_DATA,
-  })
-    .then((res) => res.json())
-    .then((res) => res) //return the res
-    .catch((err) => {
-      console.error(err)
-    })
-}
+import { httpService } from "./httpService";
 
-export const uploadVid = (ev) => {
-  //Defining our variables
-  const UPLOAD_PRESET = 'social_n_shlomi' //insert yours
-  const CLOUD_NAME = 'duajg3ah1' //insert yours
-  const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/video/upload`
-  const FORM_DATA = new FormData()
-  //Building the request body
-  FORM_DATA.append('file', ev.target.files[0]) // form data kind of key : file, add the file we get
-  FORM_DATA.append('upload_preset', UPLOAD_PRESET) // add the upload_preset
-  //Sending a post method request to Cloudinary`s API
-  return fetch(UPLOAD_URL, {
-    // add the img to Cloudinary
-    method: 'POST',
-    body: FORM_DATA,
-  })
-    .then((res) => res.json())
-    .then((res) => res) //return the res
-    .catch((err) => console.error(err))
-}
+const toBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+
+export const uploadImg = async (ev) => {
+  try {
+    const file = ev.target.files[0];
+    const base64File = await toBase64(file);
+
+    const response = await httpService.post("cloudinary/upload", {
+      file: base64File,
+      resourceType: "image",
+    });
+
+    return response;
+  } catch (err) {
+    console.error("Error uploading image:", err);
+    throw err;
+  }
+};
+
+export const uploadVid = async (ev) => {
+  try {
+    const file = ev.target.files[0];
+    const base64File = await toBase64(file);
+
+    const response = await httpService.post("cloudinary/upload", {
+      file: base64File,
+      resourceType: "video",
+    });
+
+    return response;
+  } catch (err) {
+    console.error("Error uploading video:", err);
+    throw err;
+  }
+};
